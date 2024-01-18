754162b0e3e88548548fea07f8db08b0f7fb5ec1
Iván Belio <ivanbeliogil0@gmail.com>
2024-01-18 19:42:49 +0100

    Creamos los elementos para poder referirnos a las clases

---
+++ script.js
@@ -0,0 +1,11 @@
+// Creamos los elementos para poder referirnos a las clases
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-modal')
+const btnOpenModal = document.querySelectorAll('.show.modal')
+
+// Creamos las funciones para poder abrir y cerrar la ventana
+
+
+// Cremos un evento global para poder cerrar la ventana con la tecla de esc
+
