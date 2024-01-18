e7273f0b0696825747f1b309db71e3eb78dd2f5d
Iván Belio <ivanbeliogil0@gmail.com>
2024-01-18 19:47:05 +0100

    Creamos las funciones para poder abrir y cerrar la ventana

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ const btnCloseModal = document.querySelector('.close-modal')
 const btnOpenModal = document.querySelectorAll('.show.modal')
 
 // Creamos las funciones para poder abrir y cerrar la ventana
+const openModal = function () {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
+const closeModal = function () {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+// Asociamos la función a un listener para poder abrir el modal
 
 
 // Cremos un evento global para poder cerrar la ventana con la tecla de esc
